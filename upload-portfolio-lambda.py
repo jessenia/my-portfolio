d23a8b47dfeb0b14c2af7b630cc01ae51b5ecb74
Jesssenia Kahn <jessenia.felix@gmail.com>
2017-11-19 23:10:32 -0500

    added aws sns functionality support

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,19 +2,29 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
-import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:704501493220:deployPortfolioTopic')
 
-portfolio_bucket = s3.Bucket('portfolio.jesseniakahn.com')
-build_bucket = s3.Bucket('portfoliobuild.jesseniakahn.com')
+    try:
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.jesseniakahn.com')
+    build_bucket = s3.Bucket('portfoliobuild.jesseniakahn.com')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-     for nm in myzip.namelist():
-         obj = myzip.open(nm)
-         portfolio_bucket.upload_fileobj(obj,nm,
-         ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+         for nm in myzip.namelist():
+             obj = myzip.open(nm)
+             portfolio_bucket.upload_fileobj(obj,nm)
+             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    topic.publish(Subject="Portfolio Deploy Success", Message="Copy myportfoliobuild bucket to myportfolio bucket completed successfully.")
+    except:
+    topic.publish(Subject="Portfolio Deploy Failed", Message="Copy myportfoliobuild bucket to myportfolio bucket failed.")
+    raise
+
+    return 'Hello from Lambda'
